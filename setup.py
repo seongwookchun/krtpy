aea0f0f3efbb9272c74a725d2f74cf2056cecfad
seongwookchun <40543038+seongwookchun@users.noreply.github.com>
2020-02-13 00:31:29 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='krt',
-  version='alpha1',
+  version='alpha1.0.2',
   py_modules=['krt'],
   author='Dan Rasband',
   author_email='danrasband@gmail.com',
