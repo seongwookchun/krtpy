d0e11d3b341e96d37b5dbea9019dfb27d35243c3
seongwookchun <w2da130@uos.ac.kr>
2020-03-06 03:18:04 +0900

    modified setup.py : find_packages is added

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 from distutils.core import setup
+from setuptools import find_packages
+
 setup(name='krt',
-  version='alpha1.1.0',
-  py_modules=['engine', 'utils'],
+  version='alpha1.1.2',
+  # py_modules=['engine', 'utils'],
+  package_dir={"": "krt"},
+  packages=find_packages("krt"),
   author='Dan Rasband',
   author_email='danrasband@gmail.com',
   url='http://code.google.com/p/krtpy'
