dacbb5d3fb172eec26ecc00eb09eb9b7faf6683b
seongwookchun <w2da130@uos.ac.kr>
2020-03-06 03:06:16 +0900

    module structure change/ class cstr is added

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(name='krt',
-  version='alpha1.0.2',
-  py_modules=['krt'],
+  version='alpha1.1.0',
+  py_modules=['engine'],
   author='Dan Rasband',
   author_email='danrasband@gmail.com',
   url='http://code.google.com/p/krtpy'
