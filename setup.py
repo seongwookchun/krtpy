73dfc098494de40db69cab6d0248ccbce9cd7c3c
seongwookchun <w2da130@uos.ac.kr>
2020-03-07 02:43:00 +0900

    modify setup.py: 코랩에서 에러, packages=[krt] 추가

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(name='krt',
   # py_modules=['engine', 'utils'],
   #package_dir={"": "krt"},
   #packages=find_packages("krt"),
+  packages=['krt'],
   author='Dan Rasband',
   author_email='danrasband@gmail.com',
   url='http://code.google.com/p/krtpy'
