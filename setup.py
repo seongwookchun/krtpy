0e9b3ccc8a334bf03364e6f93a3d5059aef1e2ff
seongwookchun <w2da130@uos.ac.kr>
2020-03-07 02:41:02 +0900

    modify setup.py: 코랩에서 에러, ModuleNotFoundError: No module named krtpy

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(name='krt',
-  version='alpha1.1.3',
+  version='alpha1.1.4',
   # py_modules=['engine', 'utils'],
-  package_dir={"": "krt"},
-  packages=find_packages("krt"),
+  #package_dir={"": "krt"},
+  #packages=find_packages("krt"),
   author='Dan Rasband',
   author_email='danrasband@gmail.com',
   url='http://code.google.com/p/krtpy'
